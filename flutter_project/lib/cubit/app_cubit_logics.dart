fda642bee782a16c867f43adf2951c51d090873f
Agnesia <agnesia.carpenco@isa.utm.md>
2024-09-12 20:51:07 +0300

    Update cubit folder

--- flutter_project/lib/cubit/app_cubit_logics.dart
+++ flutter_project/lib/cubit/app_cubit_logics.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:travel_app_flutter/cubit/app_cubit.dart';
+
 import 'package:travel_app_flutter/cubit/app_cubit_states.dart';
+import 'package:travel_app_flutter/cubit/app_cubits.dart';
 import 'package:travel_app_flutter/pages/navpages/detail_page.dart';
 import 'package:travel_app_flutter/pages/navpages/main_page.dart';
 import 'package:travel_app_flutter/pages/welcome_page.dart';
 
 class AppCubitLogics extends StatefulWidget {
-  const AppCubitLogics({Key? key}) : super(key: key);
+  const AppCubitLogics({super.key});
 
   @override
   State<AppCubitLogics> createState() => _AppCubitLogicsState();
@@ -17,21 +18,22 @@ class _AppCubitLogicsState extends State<AppCubitLogics> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: BlocBuilder<AppCubits, CubitStates>(builder: (context, state) {
-        if (state is WelcomeState) {
-          return const WelcomePage();
-        } else if (state is LoadingState) {
-          return const Center(
-            child: CircularProgressIndicator(),
-          );
-        } else if (state is DetailState) {
-          return const DetailPage();
-        } else if (state is LoadedState) {
-          return const MainPage();
-        } else {
-          return Container();
-        }
-      }),
-    );
+        body: BlocBuilder<AppCubits, CubitStates>(builder: (context, state) {
+      if (state is DetailState) {
+        return const DetailPage();
+        // ignore: dead_code
+      }
+      if (state is WelcomeState) {
+        return const WelcomePage();
+      }
+      if (state is LoadedState) {
+        return const MainPage();
+      }
+      if (state is LoadingState) {
+        return const Center(child: CircularProgressIndicator());
+      } else {
+        return Container();
+      }
+    }));
   }
 }
--- flutter_project/lib/cubit/app_cubit_states.dart
+++ flutter_project/lib/cubit/app_cubit_states.dart
@@ -1,50 +1,40 @@
 import 'package:equatable/equatable.dart';
 import 'package:travel_app_flutter/model/data_model.dart';
 
-abstract class CubitStates extends Equatable {
-  // Nu este nevoie să definim `props` aici, deoarece Equatable furnizează acest lucru
-}
+abstract class CubitStates extends Equatable {}
 
 class InitialState extends CubitStates {
-  final int x; // Variabilă finală
-
-  InitialState(this.x); // Constructor pentru inițializarea lui `x`
-
   @override
-  List<Object> get props => [x]; // Suprascrierea getter-ului `props`
+// TODO: implement props
+  List<Object> get props => [];
 }
 
 class WelcomeState extends CubitStates {
   @override
+  // TODO: implement props
   List<Object> get props => []; // `props` gol, fără variabile de comparat
 }
 
 class LoadingState extends CubitStates {
   @override
+  // TODO: implement props
   List<Object> get props => [];
 }
 
 class LoadedState extends CubitStates {
+  LoadedState(this.places);
   final List<DataModel> places;
 
-  LoadedState(this.places);
-
   @override
+  // TODO: implement props
   List<Object> get props => [places];
 }
 
 class DetailState extends CubitStates {
+  DetailState(this.place);
   final DataModel place;
 
-  DetailState(this.place);
-
   @override
+  // TODO: implement props
   List<Object> get props => [place];
 }
-
-
-
-
-
-
-
